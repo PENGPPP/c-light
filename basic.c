6454a021d39e69be21bce87da7583aeb8d9e280d
yangjiwei <yangjiwei@zidanduanxin.com>
2019-12-30 15:53:33 +0800

    segment & box & triangle

--- basic.c
+++ basic.c
@@ -20,20 +20,80 @@ float circleSDF(float x, float y, float cx, float cy, float r)
     return sqrtf(ux * ux + uy * uy) - r;
 }
 
+float planeSDF(float x, float y, float sx, float sy, float ex, float ey)
+{
+    // 向量[x - sx,y - sy] 和 [ex, ey] 的点积 
+    return (x - sx) * ex + (y - sy) * ey;
+}
+
+float segmentSDF(float x, float y, float ax, float ay, float bx, float by)
+{
+    float ux = bx - ax, uy = by - ay, vx = x - ax, vy = y - ay;
+    float t = fmaxf(fminf((ux * vx + uy * vy) / (ux * ux + uy * uy), 1.0f), 0.0f);
+    float dx = vx - ux * t, dy = vy - uy * t;
+    return sqrtf(dx * dx + dy * dy);
+}
+
+float capsuleSDF(float x, float y, float ax, float ay, float bx, float by, float r)
+{
+    return segmentSDF(x, y, ax, ay, bx, by) - r;
+}
+
+float boxSDF(float x, float y, float cx, float cy, float theta, float sx, float sy) {
+    float costheta = cosf(theta), sintheta = sinf(theta);
+    float dx = fabs((x - cx) * costheta + (y - cy) * sintheta) - sx;
+    float dy = fabs((y - cy) * costheta - (x - cx) * sintheta) - sy;
+    float ax = fmaxf(dx, 0.0f), ay = fmaxf(dy, 0.0f);
+    return fminf(fmaxf(dx, dy), 0.0f) + sqrtf(ax * ax + ay * ay);
+}
+
+float triangleSDF(float x, float y, float ax, float ay, float bx, float by, float cx, float cy) {
+    float d = fminf(fminf(
+        segmentSDF(x, y, ax, ay, bx, by),
+        segmentSDF(x, y, bx, by, cx, cy)),
+        segmentSDF(x, y, cx, cy, ax, ay));
+    return (bx - ax) * (y - ay) > (by - ay) * (x - ax) && 
+           (cx - bx) * (y - by) > (cy - by) * (x - bx) && 
+           (ax - cx) * (y - cy) > (ay - cy) * (x - cx) ? -d : d;
+}
+
 Result unionOp(Result a, Result b)
 {
     return a.sd > b.sd ? b : a;
 }
 
+Result intersectOp(Result a, Result b) {
+    Result r = a.sd > b.sd ? b : a;
+    r.sd = a.sd > b.sd ? a.sd : b.sd;
+    return r;
+}
+
+Result subtractOp(Result a, Result b) {
+    Result r = a;
+    r.sd = (a.sd > -b.sd) ? a.sd : -b.sd;
+    return r;
+}
 
 Result sence(float x, float y)
 {
-#if 1
+#if 0
     Result r1 = { circleSDF(x, y, 0.3f, 0.3f, 0.10f), 2.0f };
     Result r2 = { circleSDF(x, y, 0.3f, 0.7f, 0.05f), 0.8f };
     Result r3 = { circleSDF(x, y, 0.7f, 0.5f, 0.10f), 0.0f };
     return unionOp(unionOp(r1, r2), r3);
-    
+#elif 0
+    Result a = { circleSDF(x, y, 0.5f, 0.5f, 0.2f), 1.0f };
+    Result b = { planeSDF(x, y, 0.0f, 0.5f, 0.0f, 1.0f), 0.8f };
+    return intersectOp(a, b);
+#elif 0
+    Result c = {  capsuleSDF(x, y, 0.4f, 0.4f, 0.6f, 0.6f, 0.1f), 1.0f };
+    return c;
+#elif 0
+    Result d = { boxSDF(x, y, 0.5f, 0.5f, TWO_PI / 16.0f, 0.3f, 0.1f), 1.0f };
+    return d;
+#elif 1
+    Result f = { triangleSDF(x, y, 0.5f, 0.2f, 0.8f, 0.8f, 0.3f, 0.6f), 1.0f };
+    return f;
 #else
     Result r1 = { circleSDF(x, y, 0.5f, 0.5f, 0.10f), 2.0f };
     return r1;
@@ -61,9 +121,9 @@ float sample(float x, float y)
     float sum = 0.0f;
     for (int i = 0; i < N; i++)
     {
-        float a = TWO_PI * rand() / RAND_MAX;
+        // float a = TWO_PI * rand() / RAND_MAX;
         // float a = TWO_PI / N * i;
-        // float a = TWO_PI / N * (i + (float)rand() / RAND_MAX) ;
+        float a = TWO_PI / N * (i + (float)rand() / RAND_MAX) ;
         sum += trace(x, y, cosf(a), sinf(a));
     }
 
@@ -77,6 +137,6 @@ int main()
         for(int x = 0;  x< W; x++, p+=3)
             p[0] = p[1] = p[2] = (int)fminf(sample((float)x / W, (float)y / H) * 255.0f, 255.0f);
 
-    svpng(fopen("L_3_random_sampling_trace_max_step_10.png", "wb"), W, H, img, 0);
+    svpng(fopen("Triangle_random_sampling_trace_max_step_10.png", "wb"), W, H, img, 0);
     return 0;
 }
